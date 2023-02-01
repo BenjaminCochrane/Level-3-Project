cb8254a646d981e9df1c3fc7ffd00ae05bf5c1e6
Alistair Johnston <2560836j@student.gla.ac.uk>
2023-02-01 13:31:04 +0000

    Serial interface automatic opening

--- src/serial_interface.py
+++ src/serial_interface.py
@@ -16,41 +16,50 @@ class SerialInterface():
         self.serial_instance = None
         self.port = None
 
+        initialised = False
 
         for port in serial.tools.list_ports.comports():
-            print(port.device)
+            self.port = str(port.device)
+            self.serial_instance = serial.Serial(timeout = 0.25)
+            self.serial_instance.baudrate = 115200
+            self.serial_instance.port = self.port
+            self.serial_instance.open()
 
-        #if(serial.tools.list_ports.comports()):
-        for port in serial.tools.list_ports.comports():
-            if self.serial_instance:
-                try:
-                    self.port = str(port.device)
+            #This checks for ports sending data - is this an issue?
+            #Reason: We get partial packets here: can't check format
+            if self.serial_instance.readline():
+                initialised = True
+                break
 
-                    self.serial_instance = serial.Serial()
-                    self.serial_instance.baudrate = 115200
-                    self.serial_instance.port = self.port
-                    self.serial_instance.open()
+        #If we found a receiver reopen it without a timeout
+        if self.serial_instance:
+            self.serial_instance.close()
+        if initialised:
+            self.serial_instance = serial.Serial()
+            self.serial_instance.baudrate = 115200
+            self.serial_instance.port = self.port
+            self.serial_instance.open()
 
-                    assert isinstance(self.get_latest(), tuple)
-                except AssertionError:
-                    print("Port No worky")
-
-                    # If port was opened, but data was in the wrong format try a different port
-                    if self.serial_instance:
-                        self.serial_instance.close()
-
-        if not self.serial_instance:
+        if not initialised:
             print("No ports detected")
 
 
-    def get_latest(self) -> tuple:
+    def get_latest(self, _ = None) -> tuple:
         """Returns a tuple containing the node_id, time (x), and RSSI (y)"""
         if self.serial_instance:
             packet = self.serial_instance.readline()
+            print(packet.decode('utf-8'))
 
-            node_id, _ , rssi_value = packet.decode('utf-8').split('_')
+            try:
+                node_id, _ , rssi_value = packet.decode('utf-8').split('_')
 
-            return time.time()-self.start_time, int(rssi_value[:len(rssi_value)-4]), int(node_id)
+                print(node_id, _, rssi_value)
+
+            #return time.time()-self.start_time, int(rssi_value[:len(rssi_value)-4]), str(node_id)
+                return time.time()-self.start_time, int(rssi_value), str(node_id)
+
+            except ValueError:
+                return (None, None, None)
 
         raise AssertionError("No port was opened")
 
