aac19d28ff8aabe406169b33f25d15dfd800f39e
Hana El Sherbeny <hanaelsherbeny@glaroam2-173-245.wireless.gla.ac.uk>
2023-03-08 10:36:24 +0000

    gui tests

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -16,7 +16,6 @@ import time
 from PIL import Image
 from PIL import ImageGrab
 
-
 import time
 import datetime
 
@@ -27,61 +26,58 @@ class test_gui(Main):
             'animated_plot' : animated_plot,
 
         }
+
+        self.data_indices = {
+            "start_data_index" : 0,
+            "end_data_index"   : 0,
+        }
+
         pass
 
 animated_plot = AnimatedPlot()
 main = test_gui(animated_plot)
 
 def test_toggle_graphing():
+    '''Test for toggle graph functionality'''
     main.toggle_graphing()
     assert(True)
 
 def test_start_graphing():
+    '''Test for start graphing functionality'''
     main.start_graphing()
     assert(True)
 
 def test_stop_graphing():
+    '''Test for stop graphing functionality'''
     main.stop_graphing()
     assert(True)
 
+def test_stop_recording():
+    '''Test stop recording functionality'''
+    main.stop_recording()
+    assert(True)
 
+def test_saving_choices():
+    '''Test saving data options'''
+    main.stop_recording()
+    main.saving_choices()
+    assert(True)
 
+def test_save_new_file():
+    '''Test option to save data to a new file'''
+    main.save_new_file()
+    assert(True)
 
-# def test_toggle_graphing_initial_state():
-#     assert main.graph_started == False 
+def test_append_to_file():
+    '''Test option to append saved data to an existing file'''
+    main.append_to_file()
+    assert(True)
 
-# def test_toggle_graphing_initial_text():
-#     assert main.buttons.get("graph_button").cget("text") == "Start Graphing"
+def test_overwrite_file():
+    '''Test option to overwrite data in an existing file'''
+    main.overwrite_file()
+    assert(True)
 
-# def test_toggle_graphing_after_text():
-#     main.buttons.get("graph_button").invoke()
-#     assert main.get("graph_button").cget("text") == "Stop Graphing"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def test_start_recording():
-#     '''Test start_recording method that strats recoding data to be saved in .csv'''
-#     animated_plot = AnimatedPlot()
-#     main = Main(animated_plot)
-#     main.start_recording()
-#     time.sleep(1)
-#     main.stop_recording()
-#     assert main.data_frame is not None
-#     # self.assertListEqual(list(main.data_frame.columns), ["Time", "RSSI Value", "Node ID"])
-#     assert list(main.data_frame.columns) == ["Time", "RSSI Value", "Node ID"]
-
-#passed
 def test_save_data_creates_file():
     '''Test save_data method that saves data to a .csv file in the saved_files directory'''
     animated_plot = AnimatedPlot()
@@ -89,30 +85,10 @@ def test_save_data_creates_file():
     saved_files_dir = os.path.join(os.path.dirname(__file__), '..', 'saved_files')
     os.makedirs(saved_files_dir, exist_ok=True)
     file_path = os.path.join(saved_files_dir, "data-" + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + ".csv")
-    #main.saving_choices()
     assert os.path.isfile(file_path) == False
 
-#this passes yay
-#def test_start_graphing():
-#    '''Test start_graphing method that starts graphing data'''
-#    mock = Mock()
-#    animated_plot = AnimatedPlot(mock.get_latest)
-#    main = Main(animated_plot)
-#    main.start_graphing()
-#    assert main.anim_started == True
-
-#passed
-#def test_stop_graphing():
-#    '''Test stop_graphing method that stops graphing data'''
-#    mock = Mock()
-#    animated_plot = AnimatedPlot(mock.get_latest)
-#    main = Main(animated_plot)
-#    main.stop_graphing()
-#    assert main.anim_started is None
-
-
-#this passes yayyy
 def test_save_data_creates_and_appends_to_file():
+    '''Test saving data to a new file creates a new file '''
     saved_files_dir = os.path.join(os.path.dirname(__file__), '..', 'saved_files')
     os.makedirs(saved_files_dir, exist_ok=True)
     file_path = os.path.join(saved_files_dir, "data-" + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + ".csv")
@@ -144,101 +120,5 @@ def test_save_data_creates_and_appends_to_file():
 
 
 
-#main.root.destroy()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-#gvgvgv
-# def test_save_data_creates_and_appends_to_file(self):
-
-#     mock = Mock()
-#     animated_plot = AnimatedPlot(mock.get_latest)
-#     main = Main(animated_plot)
-#     saved_files_dir = os.path.join(os.path.dirname(__file__), '..', 'saved_files')
-#     os.makedirs(saved_files_dir, exist_ok=True)
-#     file_path = os.path.join(saved_files_dir, "data-" + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + ".csv")
-#     data = []
-#     for i in range(10):
-#         time, sensor, node_id = mock.get_latest()
-#         data.append((time, sensor, node_id))
-#     main.start_recording()
-#     time.sleep(1)
-#     main.stop_recording()
-#     assert os.path.isdir(self.saved_files_dir)
-
-
-
-#     # # check that the data frame was created and has the expected columns
-#     # assert isinstance(main.data_frame, pd.DataFrame)
-#     # assert set(main.data_frame.columns) == {'Time', 'RSSI Value', 'Node ID'}
-    
-#     # # check that the data frame is not empty
-#     # assert not main.data_frame.empty
-
-
-
-
-
-
-    # self.assertEqual(data[start_data_index:end_data_index], self.obj.data_frame.iloc[:, :3].values)
-
-# @patch('gui.pd') # to replace the pd library with the mock version while running the test
-# def test_stop_recording_method(self, mock_pd):
-#     """Test that the stop_recording method stops recording data and saves the data to a CSV file"""
-#     animated_plot = object()
-#     gui = Main(animated_plot)
-    
-#     # Call the stop_recording method
-#     gui.stop_recording()
-    
-#     # Check that the DataFrame constructor was called with the correct data
-#     mock_pd.DataFrame.assert_called_with(animated_plot.current_data, columns=["Time", "RSSI Value", "Node ID"])
-    
-#     # Check that the DataFrame's to_csv method was called with the correct file name
-#     mock_pd.DataFrame().to_csv.assert_called_with("data.csv")
-
-
-# def test_start_recording(self):
-#     animated_plot = AnimatedPlot()
-#     self.obj= Main(animated_plot)
-    
-#     self.obj.start_recording(self.obj)
-#     data = self.obj.animated_plot.get_current_data()
-#     end_data_index = self.obj.end_data_index
-#     start_data_index = self.obj.start_data_index
-#     self.assertLess(end_data_index-1, len(data))
-#     self.assertEqual(data[start_data_index:end_data_index], self.obj.data_frame.iloc[:, :3].values)
-
-
-
-# def test_save_data_creates_file():
-#     animated_plot = AnimatedPlot()
-#     main = Main(animated_plot)
-#     saved_files_dir = os.path.join(os.path.dirname(__file__), '..', 'saved_files')
-#     os.makedirs(saved_files_dir, exist_ok=True)
-#     file_path = os.path.join(saved_files_dir, "data-" + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + ".csv")
-#     main.start_recording()
-#     time.sleep(1)
-#     main.stop_recording()
-#     # assert os.path.isdir(self.saved_files_dir)
-#     print(file_path)
-#     assert os.path.isfile(file_path) == False
-
-
-# check that the index is in the dataframe
-# check that the start and end indices are in the dataframe
-# file is created 
-# create a file (create an actual .csv inside the ) / get its size / make sure that the new file is larger
-# check that all the data is appended and not just the length of the file
